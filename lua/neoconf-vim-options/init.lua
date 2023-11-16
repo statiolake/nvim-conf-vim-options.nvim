6973f88b2d1c48bfd0c31abea60e41ace35b47c4
statiolake <statiolake@gmail.com>
2023-11-17 01:40:49 +0900

    refactor: move modules and reformat by stylua

--- lua/neoconf-vim-options/init.lua
+++ lua/neoconf-vim-options/init.lua
@@ -1,4 +1,4 @@
-local neoconf = require'neoconf'
+local neoconf = require 'neoconf'
 
 -- {
 --   "vim-options": {
@@ -14,9 +14,11 @@ local neoconf = require'neoconf'
 local M = {}
 
 function M.apply(filetype)
-  local g_opts = neoconf.get("vim-options._", {})
-  local l_opts = filetype and neoconf.get(string.format("vim-options.%s", filetype), {}) or {}
-  local opts = vim.tbl_deep_extend("force", g_opts, l_opts) or {}
+  local g_opts = neoconf.get('vim-options._', {})
+  local l_opts = filetype
+      and neoconf.get(string.format('vim-options.%s', filetype), {})
+    or {}
+  local opts = vim.tbl_deep_extend('force', g_opts, l_opts) or {}
   for opt, value in pairs(opts) do
     vim.opt_local[opt] = value
   end
@@ -27,10 +29,11 @@ function M.on_filetype(event)
 end
 
 function M.setup()
-  local augroup_id = vim.api.nvim_create_augroup("neoconf-vim-options", { clear = true })
+  local augroup_id =
+    vim.api.nvim_create_augroup('neoconf-vim-options', { clear = true })
   vim.api.nvim_create_autocmd('FileType', {
     group = augroup_id,
-    pattern = "*",
+    pattern = '*',
     callback = M.on_filetype,
   })
   M.apply(nil)
