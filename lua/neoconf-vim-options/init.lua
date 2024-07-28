7b12ffbdb3276f415b99617dfdc102c66d513db9
statiolake <statiolake@gmail.com>
2024-07-28 13:00:23 +0900

    feat: migrate to nvim-conf

--- lua/neoconf-vim-options/init.lua
+++ lua/neoconf-vim-options/init.lua
@@ -1,25 +1,16 @@
-local neoconf = require 'neoconf'
-
--- {
---   "vim-options": {
---     "_": {
---       "textwidth": 78,
---     },
---     "python": {
---       "textwidth": 78,
---     },
---   }
+-- vim_options = nc.per_filetype_value {
+--   _ = {
+--     textwidth = 78,
+--   },
+--   python = {
+--     textwidth = 78,
+--   },
 -- }
 
 local M = {}
 
 function M.apply(filetype)
-  local g_opts = neoconf.get('vim-options._', {})
-  local l_opts = {}
-  if filetype and filetype ~= '' then
-    l_opts = neoconf.get(string.format('vim-options.%s', filetype), {})
-  end
-  local opts = vim.tbl_deep_extend('force', g_opts, l_opts) or {}
+  local opts = require 'nvim-conf'.get().vim_options or {}
   for opt, value in pairs(opts) do
     vim.opt_local[opt] = value
   end
@@ -31,7 +22,7 @@ end
 
 function M.setup()
   local augroup_id =
-    vim.api.nvim_create_augroup('neoconf-vim-options', { clear = true })
+    vim.api.nvim_create_augroup('nvim-conf-vim-options', { clear = true })
   vim.api.nvim_create_autocmd('FileType', {
     group = augroup_id,
     pattern = '*',
